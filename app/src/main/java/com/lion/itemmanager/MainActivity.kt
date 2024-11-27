a1c60df41232b0eb48309f215731a00f680b6865
KSJ <plplplplo48@gmail.com>
2024-11-27 10:34:18 +0900

    start

---
+++ app/src/main/java/com/lion/itemmanager/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.lion.itemmanager
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
+import com.google.android.material.transition.MaterialSharedAxis
+import com.lion.itemmanager.databinding.ActivityMainBinding
+import com.lion.itemmanager.fragment.LoginFragment
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    // 프래그먼트를 교체하는 함수
+    fun replaceFragment(fragmentName: FragmentName, isAddToBackStack:Boolean, animate:Boolean, dataBundle: Bundle?){
+        // 프래그먼트 객체
+        val newFragment = when(fragmentName){
+            // 로그인 화면
+            FragmentName.LOGIN_FRAGMENT -> LoginFragment()
+        }
+
+        // bundle 객체가 null이 아니라면
+        if(dataBundle != null){
+            newFragment.arguments = dataBundle
+        }
+
+        // 프래그먼트 교체
+        supportFragmentManager.commit {
+
+            if(animate) {
+                newFragment.exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ true)
+                newFragment.reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ false)
+                newFragment.enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ true)
+                newFragment.returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ false)
+            }
+
+            replace(R.id.fragmentContainerViewMain, newFragment)
+            if(isAddToBackStack){
+                addToBackStack(fragmentName.str)
+            }
+        }
+    }
+
+    // 프래그먼트를 BackStack에서 제거하는 메서드
+    fun removeFragment(fragmentName: FragmentName){
+        supportFragmentManager.popBackStack(fragmentName.str, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+    }
+}
+
+// 프래그먼트들을 나타내는 값들
+enum class FragmentName(var number:Int, var str:String){
+    // 로그인 화면
+    LOGIN_FRAGMENT(1, "LoginFragment"),
+}
